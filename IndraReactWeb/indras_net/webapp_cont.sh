3c6e5fb9cc094f8aecf205c05f3c6741b3c1a04d
gcallah <gcallah@mac.com>
2021-03-08 17:30:55 -0500

    Eliminating indras_net

--- IndraReactWeb/indras_net/webapp_cont.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker rm webapp-dev-container 2> /dev/null || true
-docker run --rm -p 3000:3000 -v `pwd`/webapp/public:/home/public -v `pwd`/webapp/src:/home/src --name webapp-dev-container gcallah/indras_webapp
--- IndraReactWeb/indras_net/webapp_icont.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --rm -it -p 3000:3000 -v `pwd`/webapp/public:/home/public -v `pwd`/webapp/src:/home/src --name webapp-dev-container gcallah/indras_webapp sh
