1c12829eaae621fcdb298560f1d6f5ed4a8c7ca3
Rachel Tao <rt1726@nyu.edu>
2021-04-07 22:43:08 -0400

    Add RunModelButton.js

---
+++ IndraReactNative/components/RunModelButton.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+class RunModelButton extends Component {
+    constructor(props) {
+        super(props);
+        const {   } = this.props;
+    }
+
+    render() {
+
+    }
+}
+
+export default RunModelButton;
