cf1f53dc196b0cea6bc8b8eafe99412219e5f28f
Rachel Tao <rt1726@nyu.edu>
2021-04-09 23:59:43 -0400

    remove exec_key param from menu request

--- IndraReactNative/components/ModelView.js
+++ IndraReactNative/components/ModelView.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
-import { View, Text, Dimensions, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import { Button, ButtonGroup } from 'react-native-elements';
-import axios from 'axios'
+import axios from 'axios';
 import config from '../../IndraReactCommon/config'
 import { ScrollView } from 'react-native-gesture-handler';
+import RunModelButton from './RunModelButton';
 
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
@@ -11,10 +12,11 @@ var height = Dimensions.get('window').height;
 class ModelView extends Component {
     constructor(props) {
         super(props);
-        const { route, navigation } = this.props
-        this.state = {modelParams: route.params.modelParams, modelID: route.params.modelID, modelName: route.params.modelName, selectedModel: 0, ready: false, modelWorking: true}
+        const { route, navigation } = this.props;
+        this.state = {modelParams: route.params.modelParams, modelID: route.params.modelID, modelName: route.params.modelName, selectedModel: 0, ready: false, modelWorking: true};
         this.props_url = config.PROPS_URL;
         this.menu_url = config.MENU_URL;
+        this.run_url = config.RUN_URL;
         this.updateModelId = this.updateModelId.bind(this);
         this.modelExist = this.modelWorking.bind(this);
     }
@@ -22,14 +24,16 @@ class ModelView extends Component {
     async componentDidMount(){
         var temp;
         let params = axios
-        .put(`${this.props_url}${this.state.modelID}`, this.state.modelParams)
+        .put(`${this.props_url}${this.state.modelID}`, this.state.modelParams);
         .then((response) => {
             temp = response.data
-            this.setState({execKey: temp.exec_key})
-            return axios.get(`${this.menu_url}${this.state.execKey}`)
+            this.setState({env: temp, execKey: temp.exec_key});
+            return axios.get(`${this.menu_url}`);
         })
         .then((response) => {
+            console.log(response.data[0]);
             temp = JSON.stringify(response.data);
+            
             this.setState({models: temp, ready: true});
             //console.log("active:", response.data[0].active, response.data[1].active, response.data[2].active, response.data[3].active, response.data[4].active, response.data[5].active, response.data[6].active)
             if (response.data[1].active === false) this.setState({modelWorking: false});
@@ -49,7 +53,15 @@ class ModelView extends Component {
         //console.log("active?", obj[modelId+1].active, obj[modelId+1].func);
         //console.log("active status:", obj[0].active, obj[1].active, obj[2].active, obj[3].active, obj[4].active, obj[5].active, obj[6].active, "\n");
         if (obj[modelId+1].active === false) this.setState({modelWorking: false});
-        else this.setState({modelWorking: true})
+        else this.setState({modelWorking: true});
+    }
+
+    sendNumPeriods = async () => {
+
+    }
+
+    handleRunPeriod = (e) => {
+
     }
 
 
@@ -60,8 +72,8 @@ class ModelView extends Component {
         var buttons = [];
         if (this.state.models != undefined) {
             var obj = JSON.parse(this.state.models);
-            
-            for(let i = 1; i < obj.length - 1; i++){
+            for(let i = 2; i < 7; i++){
+                //console.log("i:", i);
                 buttons.push(obj[i].func);
                 //console.log("adding func:", obj[i].func);
             }
@@ -114,7 +126,7 @@ class ModelView extends Component {
                     <Text>SelectedId:{this.state.selectedModel+2}</Text>
                     <Text>ModelWorking:{String(this.state.modelWorking)}</Text>
                     <Text>{"\n"}</Text>
-                    <Text>Models:{this.state.models}</Text>
+                    
                 </ScrollView>
             </View>
         )}
