328ab5d0899329a178346da0fe249d3ade009b92
SolidifiedRay <yc3346@nyu.edu>
2021-04-17 17:24:41 -0700

    Use the get pophist endpoint
    Co-authored-by: ZijieXiong <zx811@nyu.edu>

--- IndraReactWeb/src/components/PopulationGraph.js
+++ IndraReactWeb/src/components/PopulationGraph.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { LineChart } from 'react-chartkick';
 import 'chart.js';
 import PropType from 'prop-types';
 import CardWrapper from './CardWrapper';
+import axios from 'axios';
+import config from 'IndraReactCommon/config';
+
+const POPHIST_URL  = config.POPHIST_URL ;
 
 function PopulationGraph(props) {
+  const [popHist, setPopHist] = useState({});
+  const { loadingData, envFile } = props;
+
+  useEffect(() => {
+    async function fetchPopHist() {
+      const EXEC_KEY = envFile.exec_key;
+      const res = await axios.get(`${POPHIST_URL}${EXEC_KEY}`);
+      setPopHist(res.data.pops);
+    }
+
+    fetchPopHist();
+  }, [props]);
+
   const NUM_COLORS = 7;
   const colors = ['red', 'green', 'blue', 'black', 'purple', 'magenta', 'orange'];
   let thisColor = 0;
-  const { loadingData, envFile } = props;
   if (loadingData) {
     const data = [];
-    const env = props.envFile.pop_hist.pops;
+    console.log(popHist);
+    const env = popHist;
     // populate 'data' array with groups from 'pops'
     // and their respective values
     Object.keys(env).forEach((group, iGroup) => {
       data.push({
         name: group,
-        // color: colors[thisColor % NUM_COLORS],
-        color: envFile.members[group]
-          ? envFile.members[group].attrs.color : colors[thisColor % NUM_COLORS],
+        color: colors[thisColor % NUM_COLORS],
+        //color: envFile.members[group]
+          //? envFile.members[group].attrs.color : colors[thisColor % NUM_COLORS],
         data: {},
       });
       // modify individual 'data' dictionary of each pops
