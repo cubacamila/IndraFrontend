9eefe87fa4a33b92e3819fa3b23847009544e520
SolidifiedRay <yc3346@nyu.edu>
2021-05-01 20:45:54 -0700

    Add a placeholder for the DebugMenu component
    Co-authored-by: ZijieXiong <zx811@nyu.edu>

---
+++ IndraReactWeb/src/components/DebugMenu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class DebugMenu extends Component {
+  render() {
+    
+    return (
+      <h1>Debug menu go here</h1>
+    );
+  }
+}
+
+export default DebugMenu;
